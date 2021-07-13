d5dfa72eeddd67eb4d98b82e5fb6a796c01d7a66
Ibrahim Waheed <Ibrahimwaheed@hotmail.co.uk>
2021-07-13 13:54:38 +0100

    setup asp database and model for the payment system now onto CRUD operation the asp is using SQL database

---
+++ PaymentSystem/PaymentSystem/Models/PaymentDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+// this is Model
+
+namespace PaymentSystem.Models
+{
+    public class PaymentDetails
+    {
+        // mask the primay KEY
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")] 
+        public string CardName { get; set; }
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; }
+
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string  ExpireDate { get; set; }
+
+        [Column(TypeName = "nvarchar(3)")]
+        public  string SecurityCode { get; set; }
+
+    }
+}
---
+++ PaymentSystem/PaymentSystem/Models/PaymentDetailsContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentSystem.Models
+{
+    public class PaymentDetailsContext : DbContext
+    {
+        public PaymentDetailsContext(DbContextOptions<PaymentDetailsContext> options) : base(options)
+        { 
+        
+      
+
+        }
+
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+
+    }
+}
